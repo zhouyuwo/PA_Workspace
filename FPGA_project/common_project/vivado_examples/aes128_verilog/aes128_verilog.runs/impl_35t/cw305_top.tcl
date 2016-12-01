6ad107e69cf5c0e7f837257d4e2623a3c6708e4a
zhouyuwo <z179084212@gmail.com>
2016-12-01 18:48:42 +0800

    第一次完整

---
+++ FPGA_project/common_project/vivado_examples/aes128_verilog/aes128_verilog.runs/impl_35t/cw305_top.tcl
@@ -0,0 +1,76 @@
+proc start_step { step } {
+  set stopFile ".stop.rst"
+  if {[file isfile .stop.rst]} {
+    puts ""
+    puts "*** Halting run - EA reset detected ***"
+    puts ""
+    puts ""
+    return -code error
+  }
+  set beginFile ".$step.begin.rst"
+  set platform "$::tcl_platform(platform)"
+  set user "$::tcl_platform(user)"
+  set pid [pid]
+  set host ""
+  if { [string equal $platform unix] } {
+    if { [info exist ::env(HOSTNAME)] } {
+      set host $::env(HOSTNAME)
+    }
+  } else {
+    if { [info exist ::env(COMPUTERNAME)] } {
+      set host $::env(COMPUTERNAME)
+    }
+  }
+  set ch [open $beginFile w]
+  puts $ch "<?xml version=\"1.0\"?>"
+  puts $ch "<ProcessHandle Version=\"1\" Minor=\"0\">"
+  puts $ch "    <Process Command=\".planAhead.\" Owner=\"$user\" Host=\"$host\" Pid=\"$pid\">"
+  puts $ch "    </Process>"
+  puts $ch "</ProcessHandle>"
+  close $ch
+}
+
+proc end_step { step } {
+  set endFile ".$step.end.rst"
+  set ch [open $endFile w]
+  close $ch
+}
+
+proc step_failed { step } {
+  set endFile ".$step.error.rst"
+  set ch [open $endFile w]
+  close $ch
+}
+
+set_msg_config -id {HDL 9-1061} -limit 100000
+set_msg_config -id {HDL 9-1654} -limit 100000
+set_msg_config -id {Synth 8-256} -limit 10000
+set_msg_config -id {Synth 8-638} -limit 10000
+
+start_step write_bitstream
+set rc [catch {
+  create_msg_db write_bitstream.pb
+  open_checkpoint cw305_top_routed.dcp
+  set_property webtalk.parent_dir D:/PA_Workspace/FPGA_project/common_project/vivado_examples/aes128_verilog/aes128_verilog.cache/wt [current_project]
+  set src_rc [catch { 
+    puts "source D:/PA_Workspace/write_bitstream.tcl"
+    source D:/PA_Workspace/write_bitstream.tcl
+  } _RESULT] 
+  if {$src_rc} { 
+    send_msg_id runtcl-1 error "$_RESULT"
+    send_msg_id runtcl-2 error "sourcing script D:/PA_Workspace/write_bitstream.tcl failed"
+    return -code error
+  }
+  catch { write_mem_info -force cw305_top.mmi }
+  write_bitstream -force cw305_top.bit 
+  catch { write_sysdef -hwdef cw305_top.hwdef -bitfile cw305_top.bit -meminfo cw305_top.mmi -file cw305_top.sysdef }
+  catch {write_debug_probes -quiet -force debug_nets}
+  close_msg_db -file write_bitstream.pb
+} RESULT]
+if {$rc} {
+  step_failed write_bitstream
+  return -code error $RESULT
+} else {
+  end_step write_bitstream
+}
+
